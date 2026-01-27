81f2c00f55f032ea78aa67981ae5a5a83578bbaa
haengguk <guddnrb14@naver.com>
2026-01-27 17:27:21 +0900

    chore: [common] 초기 세팅

---
+++ src/main/java/com/edurican/enchelin/EnchelinApplication.java
@@ -0,0 +1,13 @@
+package com.edurican.enchelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchelinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnchelinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edurican/enchelin/EnchelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edurican.enchelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnchelinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
